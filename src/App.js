9036c3fdb545c2af8c523b249915df67cbfd271d
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2023-09-12 12:43:16 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ async function transfer() {
      
       result = await transferToken(toAddress, contract, quantity);
 
-    setMessage(JSON.stringify(result));
+    setMessage(JSON.stringify.hash(result));
   }
  
 
