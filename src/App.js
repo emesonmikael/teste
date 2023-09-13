c07efae15551cebe6d4ae21c54dca74fba0dd32f
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2023-09-13 12:24:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import App2 from './App2.js';
+
 import {ethers} from 'ethers';
 import {useState} from 'react';
 import { getTokenBalance, getBnbBalance, transferBnb, transferToken, getTransaction } from './MetaMaskService';
@@ -151,6 +151,75 @@ Preço : 20 EIT
 
             )
 
+        }
+        {
+          error ? <p>{error}</p> : <></>
+        }
+      </div>
+    </header>
+  </div>
+  );
+}
+function App2() {
+const [error, setError] = useState('');
+
+async function doSignIn(){ }
+
+const [wallet, setWallet] = useState('');
+ 
+async function doSignUp(){
+  setError('');
+ 
+  if (!window.ethereum) return setError(`No MetaMask found!`);
+ 
+  try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.send("eth_requestAccounts", []);
+    if (!accounts || !accounts.length) return setError('Wallet not found/allowed!');
+ localStorage.setItem('wallet', accounts[0]);
+ 
+  setWallet(accounts[0]);
+  }catch(err){
+     setError(err.message);
+  }
+}
+function doLogout(){
+  localStorage.removeItem('wallet');
+  setWallet('');
+  setError('');
+}
+  return (
+     <div className="App">
+    <header className="App-header">
+      
+      <div>
+        {
+          !wallet
+            ? (
+              <>
+		<p>
+		<h1>Login</h1>
+		</p>
+                <button onClick={doSignIn}>
+                  Sign In with MetaMask
+                </button>
+                <button onClick={doSignUp}>
+                  entre  with MetaMask
+                </button>
+              </>
+            )
+            : (
+              <>
+                <p>
+                  Wallet: {wallet}
+                </p>
+                <button onClick={doLogout}>
+                deu ceto
+                </button>
+
+              </>
+
+            )
         }
         {
           error ? <p>{error}</p> : <></>
