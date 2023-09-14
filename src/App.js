f638ccc6973d6bebd703a3adcc44d16cde0db117
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2023-09-14 13:47:10 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const [quantity, setQuantity] = useState("");
 const [message2, setMessage2] = useState('');
 const [error, setError] = useState('');
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-	const accounts = await provider.send("eth_requestAccounts", []);
+	
 
 async function checkBalance() {
       
