675c2f14454913ff993e27f4d41451468d38d5ef
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2023-09-18 12:09:57 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ const [total,setTotal] = React.useState();
 const [message2, setMessage2] = useState('');
 const [error, setError] = useState('');
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-	async function calfrete(){
+	const calfrete = ()=>{
 		const total = quantity + frete;
 		setQuantity(total);
 		setMessage2(total);
 		transfer();
-	}
+	};
 
 async function transfer() {
     let result;
