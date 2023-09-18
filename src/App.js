514a30faa9a3c147fc06a053bd3871fabbfc5843
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2023-09-18 13:03:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
 	
 const [address, setAddress] = useState("");
 const [toAddress, setToAddress] = useState("0x725e02D671AA828515e4080e97D0679eB3E867Ac");
-const [quantity, setQuantity] =React.useState(0);
+const [quantity, setQuantity] =React.useState("");
 const [frete, setFrete] =React.useState(7);
-const [total,setTotal] = React.useState();
+const [total,setTotal] = React.useState("");
  const [contract, setContract] = useState("0x6c4fbe6aefd6e2f541c99fc807436fc152af14bf");
   const [myAddress, setMyAddress] = useState("");
   const [balance, setBalance] = useState('');
