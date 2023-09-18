cd72fae94ae3a299ec53f9b8e757c647c33a8a77
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2023-09-18 13:23:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
 const [address, setAddress] = useState("");
 const [toAddress, setToAddress] = useState("0x725e02D671AA828515e4080e97D0679eB3E867Ac");
 const [quantity, setQuantity] =React.useState("");
-const [frete, setFrete] =React.useState("7");
-const [total,setTotal] = React.useState("");
+const [frete, setFrete] =React.useState(7);
+const [total,setTotal] = React.useState(0);
  const [contract, setContract] = useState("0x6c4fbe6aefd6e2f541c99fc807436fc152af14bf");
   const [myAddress, setMyAddress] = useState("");
   const [balance, setBalance] = useState('');
@@ -22,7 +22,6 @@ const provider = new ethers.providers.Web3Provider(window.ethereum);
 	const sum = ()=>{
 	const total = quantity + frete;
 	setTotal(total);
-	setQuantity(total);
 	setMessage2(total);
 	transfer();
 	};
